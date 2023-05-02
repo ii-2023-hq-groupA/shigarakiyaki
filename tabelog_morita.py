d858f9bcd9ebb54defc0f9ee5153f02ec105254f
shigarakiyaki <shigarakiyaki2000@gmail.com>
2023-05-02 11:12:08 +0900

    add data

--- tabelog_morita.py
+++ tabelog_morita.py
@@ -31,7 +31,7 @@ class Tabelog:
             return False
 
         soup = BeautifulSoup(r.content, 'html.parser') # データ抽出,子要素の取得
-        soup_a_list = soup.find_all('a', class_='list-rst__rst-name-target', limit=2) # 店名一覧
+        soup_a_list = soup.find_all('a', class_='list-rst__rst-name-target', limit=10) # 店名一覧
 
         if len(soup_a_list) == 0:
             return False
@@ -116,6 +116,7 @@ class Tabelog:
 def main():
     tokyo_ramen_review = Tabelog(base_url="https://tabelog.com/tokyo/A1301/rstLst/ramen/1/?SrtT=rt&Srt=D&sort_mode=1")
     # A1301 ~ A1331 が　東京の地域別
+    # C13101 ~ C13123 が　東京の23区
     # Rから始まる路線別もあるがコードが並んでいない e.g. 山手線=R9、日比谷線=R1096)
     # ramenの後の数字がページ番号 (最大でも60pしか表示されないらしい)
     # ランキングでソートしている
